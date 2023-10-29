c9819cf7fd7bde2c0a684c680c5711133c8bac1e
Gemmus <decadentcrown@gmail.com>
2023-10-29 21:51:55 +0200

    Initial commit

--- Exercise1/main.c
+++ Exercise1/main.c
@@ -19,74 +19,147 @@
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
 
-void ledInitalizer(const uint led);
-void buttonInitializer(const uint button);
+#define D1_LED 22
+#define D2_LED 21
+#define D3_LED 20
+#define SW0 9 // icreases brightness gradually if held; only in ON state
+#define SW1 8 // ON - OFF
+#define SW2 7 // decreases brightness gradually if held; only in ON state
+#define BUTTON_FILTERING 5
+#define RELEASED 1
+
+void ledInitalizer();
+void buttonInitializer();
+void pwnInitializer();
+void allLedOn();
+void allLedOff();
+bool repeating_timer_callback(struct repeating_timer *t);
 bool buttonPressed(const uint gpio);
+
+uint buttonstate = 0;
+volatile bool buttonreleased = false;
 
 int main(void) {
 
-    const uint led21 = 21;
-    const uint sw0 = 9;
-    const uint sw1 = 8;
-    const uint sw2 = 7;
-    uint blink_count = 0;
-    uint state = 0; // for SW1 ON-OFF --> ON = 1; OFF = 0;
-
+    static bool ledstate = false;
+//    uint state = 0; // for SW1 ON-OFF --> ON = 1; OFF = 0;
+    struct repeating_timer timer;
     // Initialize input output
     stdio_init_all();
 
     // Initialize LED pin
-    ledInitalizer(led21);
+    ledInitalizer();
 
     // Initialize LED pin
-    buttonInitializer(sw0);
-    buttonInitializer(sw1);
-    buttonInitializer(sw2);
+    buttonInitializer();
 
+    add_repeating_timer_ms(10, repeating_timer_callback, NULL, &timer);
     // Loop forever
     while (true){
 
-        if  (state == 1) {
-            printf("Blinking! %u\r\n", ++blink_count);
-            gpio_put(led21, true);
-            sleep_ms(1000);
-            gpio_put(led21, false);
-            sleep_ms(1000);
-        } else {
-            gpio_put(led21, false);
-            sleep_ms(1000);
+        if (buttonreleased)
+        {
+            buttonreleased = false;
+            if(ledstate)
+                ledstate = false;
+            else
+                ledstate = true;
         }
-
-        if (!gpio_get(sw1)) {
-            bool changed = buttonPressed(sw1);
-            if (true == changed) {
-                if (state == 0) {
-                    state = 1;
-                } else {
-                    state = 0;
+        if (ledstate) {
+            allLedOn();
+        } else {
+            allLedOff();
+        }
+#if 0
+        if (!gpio_get(SW1)) {
+            uint pressed = 1;
+            while(pressed == 1) {
+                bool changed = buttonPressed(SW1);
+                if (true == changed) {
+                    if (state == 0) {
+                        state = 1;
+                        pressed = 0;
+                    } else {
+                        state = 0;
+                        pressed = 0;
+                    }
                 }
             }
         }
-
+#endif
     }
 }
 
-void ledInitalizer(const uint led) {
-    gpio_init(led);
-    gpio_set_dir(led, GPIO_OUT);
+void ledInitalizer() {
+    gpio_init(D3_LED);
+    gpio_set_dir(D3_LED, GPIO_OUT);
+    gpio_init(D2_LED);
+    gpio_set_dir(D2_LED, GPIO_OUT);
+    gpio_init(D1_LED);
+    gpio_set_dir(D1_LED, GPIO_OUT);
 }
 
-void buttonInitializer(const uint button) {
-    gpio_init(button);
-    gpio_set_dir(button, GPIO_IN);
-    gpio_pull_up(button);
+void buttonInitializer() {
+    gpio_init(SW0);
+    gpio_set_dir(SW0, GPIO_IN);
+    gpio_pull_up(SW0);
+    gpio_init(SW1);
+    gpio_set_dir(SW1, GPIO_IN);
+    gpio_pull_up(SW1);
+    gpio_init(SW2);
+    gpio_set_dir(SW2, GPIO_IN);
+    gpio_pull_up(SW2);
 }
 
+void pwnInitializer() {
+    //gpio_set_function(D3_LED, GPIO_FUNC_PWM); // Slice: 2A
+    gpio_set_function(D2_LED, GPIO_FUNC_PWM); // Slice: 2B
+    // gpio_set_function(D1_LED, GPIO_FUNC_PWM); // Slice: 3A
+}
+
+void allLedOn() {
+    //gpio_put(D3_LED, true);
+    gpio_put(D2_LED, false);
+    //gpio_put(D1_LED, true);
+}
+
+void allLedOff() {
+    //gpio_put(D3_LED, false);
+    gpio_put(D2_LED, true);
+    //gpio_put(D1_LED, false);
+}
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+
+    uint new_state;
+    static uint filtercounter = 0;
+
+    new_state = gpio_get(SW1);
+    if (buttonstate != new_state)
+    {
+       if (++filtercounter >= BUTTON_FILTERING)
+       {
+           buttonstate = new_state;
+           filtercounter = 0;
+           if (new_state == RELEASED)
+           {
+               buttonreleased = true;
+           }
+       }
+    }
+    else
+    {
+        filtercounter = 0;
+    }
+
+    return true;
+}
+#if 0
 bool buttonPressed(const uint gpio) {
     int press = 0;
     int release = 0;
 
-    while(press < 2 && release < 2) {
+    while(press < 3 && release < 3) {
         if(!gpio_get(gpio)) {
             press++;
             release = 0;
@@ -103,3 +176,4 @@ bool buttonPressed(const uint gpio) {
         return false;
     }
 }
+#endif
