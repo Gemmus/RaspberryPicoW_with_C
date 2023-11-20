851d25a20fb361388abf9f1a411670124e0e12ef
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-20 20:11:22 +0200

    Update main.c

--- Exercise3/main.c
+++ Exercise3/main.c
@@ -11,22 +11,28 @@ The program should work as follows:
    and goes back to step 1. DevEui contains 8 bytes that the module outputs in hexadecimal separated by colons.
    The program must remove the colons between the bytes and convert the hexadecimal digits to lower case.
 5. Go to step 1.
+
 See page 10 in LoRa-E5 AT Command Specification_V1.0.pdf in the Oma workspace/Documents/Project for information about
 the structure of the commands. See page 24 for details of the commands needed in this assignment.
+
 Processing of DevEui is needed for registering the module to Metropolia LoRaWAN network.
 The server needs the DevEui in the specified above format to register the device to the network.
 In the project you need you register your device LoRaWAN to send data.
+
 For example:
 • Module sends DevEui:
           +ID: DevEui, 2C:F7:F1:20:32:30:A5:70
 • Program prints:
           2cf7f1203230a570
+
 Hints:
 First implement a function that reads a string from UART with a timeout. The function should wait until
 linefeed is received or timeout occurs. See implementation of uart_is_readable_within_us in
 PicoSDK for the principle of implementing a timeout.
+
 Step described above can also be considered states of a state machine. For example, the state machine
 could be implemented as shown below:
+
 switch(state) {
     case 1:
         // do what needs to be done in state1
@@ -216,7 +222,11 @@ void buttonInit() {
 }
 
 void ledsInit() {
-	@@ -230,8 +187,19 @@ void ledsInit() {
+    gpio_init(D3);
+    gpio_set_dir(D3, GPIO_OUT);
+    gpio_init(D2);
+    gpio_set_dir(D2, GPIO_OUT);
+    gpio_init(D1);
     gpio_set_dir(D1, GPIO_OUT);
 }
 
@@ -225,7 +235,35 @@ void pwmInit() {
     pwm_config config = pwm_get_default_config();
 
     // D1:             (2A)
-	@@ -267,53 +235,124 @@ void pwmInit() {
+    uint d1_slice = pwm_gpio_to_slice_num(D1);
+    uint d1_chanel = pwm_gpio_to_channel(D1);
+    pwm_set_enabled(d1_slice, false);
+    pwm_config_set_clkdiv_int(&config, DIVIDER);
+    pwm_config_set_wrap(&config, PWM_FREQ - 1);
+    pwm_init(d1_slice, &config, false);
+    pwm_set_chan_level(d1_slice, d1_chanel, LEVEL + 1);
+    gpio_set_function(D1, GPIO_FUNC_PWM);
+    pwm_set_enabled(d1_slice, true);
+
+    // D2:             (2B)
+    uint d2_slice = pwm_gpio_to_slice_num(D2);
+    uint d2_chanel = pwm_gpio_to_channel(D2);
+    pwm_set_enabled(d2_slice, false);
+    pwm_config_set_clkdiv_int(&config, DIVIDER);
+    pwm_config_set_wrap(&config, PWM_FREQ - 1);
+    pwm_init(d2_slice, &config, false);
+    pwm_set_chan_level(d2_slice, d2_chanel, LEVEL + 1);
+    gpio_set_function(D2, GPIO_FUNC_PWM);
+    pwm_set_enabled(d2_slice, true);
+
+    //D3:              (3A)
+    uint d3_slice = pwm_gpio_to_slice_num(D3);
+    uint d3_chanel = pwm_gpio_to_channel(D3);
+    pwm_set_enabled(d3_slice, false);
+    pwm_config_set_clkdiv_int(&config, DIVIDER);
+    pwm_config_set_wrap(&config, PWM_FREQ - 1);
+    pwm_init(d3_slice, &config, false);
+    pwm_set_chan_level(d3_slice, d3_chanel, LEVEL + 1);
     gpio_set_function(D3, GPIO_FUNC_PWM);
     pwm_set_enabled(d3_slice, true);
 
